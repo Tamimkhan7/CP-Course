32bf33370697f76f4ba863c55d19d5c1825ddec3
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-11-30 14:23:49 +0600

    second commit

---
+++ class five/setbit.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<bitset<31>(a)<<'\n';
+}
--- class four/contest/P - Prefix Sum Addicts.cpp
+++ class four/contest/P - Prefix Sum Addicts.cpp
@@ -1,21 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-const int N=1e5+5;
-int px[N];
 
 void solve()
 {
     int n,k;
     cin>>n>>k;
-    int a[k+1];
-    for(int i=1; i<=k; i++)cin>>a[i];
-    int ans = a[1]-a[0];
-    int x = n-k+1;
-    while(x!=0){
-
-
+    int s[n+1], a[n+1];
+    for(int i=n-k+1; i<=n; i++)cin>>s[i];
+    if(k==1)
+    {
+        cout<<"YES"<<endl;
+        return;
     }
+    for(int i=n-k+2; i<=n; i++)
+    {
+        a[i]=s[i]-s[i-1];
+    }
+    if(!is_sorted(a+n-k+2, a+n+1))
+    {
+        cout<<"NO"<<'\n';
+        return;
+    }
+    if(s[n-k+1]>1LL*a[n-k+2]*(n-k+1))
+    {
+        cout<<"NO"<<'\n';
+        return;
+    }
+    cout<<"YES"<<'\n';
 
 }
 int main()
