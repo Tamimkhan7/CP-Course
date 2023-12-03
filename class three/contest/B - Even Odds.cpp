3ae00210dcf762ceb7f4fd053e468bc44c75cb8d
Md. Tamim khan <mdtamimkhan842@gmail.comgit config --global user.name mdtamimkhan842omit --global to set the identity only in the repository$ git config --global user.email mdtamimkhan842@gmail.com>
2023-12-03 22:09:30 +0600

    second commit

--- class three/contest/B - Even Odds.cpp
+++ class three/contest/B - Even Odds.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
+int main()
+{
     long long n, k;
     cin >> n >> k;
-
-    if (k <= (n + 1) / 2) {
-        cout << 2 * k - 1 << endl;
-    } else {
-        cout << 2 * (k - (n + 1) / 2) << endl;
-    }
-
-    return 0;
+    if (k <= (n + 1) / 2)
+        cout << 2 * k - 1 << '\n';
+    else
+        cout << 2 * (k - (n + 1) / 2) << '\n';
 }
