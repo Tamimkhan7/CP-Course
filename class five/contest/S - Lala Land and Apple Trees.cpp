6ed4ed20603e406d55374d769216253c1e40c806
Tamim khan <mdtamimkhan842@gmail.com>
2024-01-19 00:58:42 +0600

    second commit

--- class five/contest/S - Lala Land and Apple Trees.cpp
+++ class five/contest/S - Lala Land and Apple Trees.cpp
@@ -22,75 +22,35 @@ int32_t main()
     int n;
     cin >> n;
     deque<pair<int, int>> d;
-    vector<pair<int, int>> v, v1;
+    vector<pair<int, int>> v;
     while (n--)
     {
         int x, y;
         cin >> x >> y;
         v.push_back(make_pair(x, y));
-        v1.push_back(make_pair(x, y));
     }
-    // sort(v.begin(), v.end(), cmp);
-    sort(v.begin(), v.end(), cmp1);
-    int ans = 0;
-    int N = 200, flag = 0;
-    while (N--)
-    {
-        int x = v.front().first;
-        for (int j = 1; j < v.size(); j++)
-        {
-            int y = v[j].first;
-            if (x < 0)
-            {
-                if (y > 0)
-                {
-                    ans += v[j].second;
-                    ans += v.front().second;
-                    v.erase(v.begin() + j);
-                    v.erase(v.begin());
-                    flag = 1;
-                    break;
-                }
-            }
-            else
-            {
-                if (y < 0)
-                {
-                    ans += v[j].second;
-                    ans += v.front().second;
-                    v.erase(v.begin() + j);
-                    v.erase(v.begin());
-                    flag = 1;
-                    break;
-                }
-            }
-        }
-    }
-    if (v.size() != 0)
-        ans += v.front().second;
-    if (flag == 1)
-        cout << ans << '\n';
-    else
-        cout << v1.front().second << '\n';
+    sort(v.begin(), v.end(), cmp);
+    // sort(v.begin(), v.end(), cmp1);
     // for (auto [x, y] : v)
     // {
     //     cout << x << ' ' << y << '\n';
     // }
-    // for (auto a : v)
-    // {
-    //     d.push_back(a);
-    // }
-    // int ans = 0, flag = 0;
-    // while ((d.front().first < 0 and d.back().first > 0) and d.size() > 1)
-    // {
-    //     ans += d.front().second;
-    //     ans += d.back().second;
-    //     d.pop_front();
-    //     d.pop_back();
-    //     flag = 1;
-    // }
-    // if (d.size() != 0)
-    //     ans += d.front().second;
-
-    // cout << ans << '\n';
+    for (auto a : v)
+    {
+        d.push_back(a);
+    }
+    int ans = 0, flag = 0;
+    while ((d.front().first < 0 and d.back().first > 0) and d.size() > 1)
+    {
+        ans += d.front().second;
+        ans += d.back().second;
+        d.pop_front();
+        d.pop_back();
+        flag = 1;
+    }
+    if (d.size() != 0 and flag == 0)
+        ans += d.front().second;
+    else if (d.size() != 0 and flag == 1)
+        ans += d.front().second;
+    cout << ans << '\n';
 }
