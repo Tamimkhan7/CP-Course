98fa04c99fe48cbd71a0dae86ac43b0c6b990f14
Tamim khan <mdtamimkhan842@gmail.com>
2024-04-20 13:37:16 +0600

    added modified

--- Class Six/contest/Chat Ban.cpp
+++ Class Six/contest/Chat Ban.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define MTK                       \
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
@@ -9,50 +8,35 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define ll long long
 #define mod 1000000007
-int k;
-ll x;
-ll get_sum(ll n)
-{
-    return n * (n + 1) / 2;
-}
-ll get_range_sum(ll l, ll r)
-{
-    // cout << l << ' ' << r << ' ';
-    // ll flag = get_sum(r) - get_sum(l - 1);
-    // cout << flag << '\n';
-    return  get_sum(r) - get_sum(l - 1);
-}
-
-bool ok(ll i)
-{
-    // cout << i << ' ';
-    ll sum = 0;
-    if (i <= k)
-        sum = get_range_sum(1, i);
-    else
-        sum = get_range_sum(1, k) + get_range_sum(k - (i - k), k - 1);
-    return sum >= x;
-}
-
 void solve()
 {
-    cin >> k >> x;
-    int l = 1, r = 2 * k - 1, ans = 2 * k - 1;
-    while (l <= r)
+    int k,cnt=0;
+    ll x;
+    cin>>k>>x;
+    vector<ll>v;
+    ll sum=0;
+    for(int i=1; i<=k; i++)
     {
-        // cout << l << ' ' << r << ' ';
-        int mid = (1LL * l + r) / 2;
-        if (ok(mid))
+        v.push_back(i);
+        sum +=i;
+        cnt++;
+        if(sum>=x)
         {
-            ans = mid;
-            r = mid - 1;
+            cout<<cnt<<'\n';
+            return;
         }
-        else
-            l = mid + 1;
     }
-    cout << ans << '\n';
+    for(int i=k-1; i>=1; i--)
+    {
+        sum +=i;
+        cnt++;
+        if(sum>=x)
+        {
+            cout<<cnt<<'\n';
+            return;
+        }
+    }
 }
-
 int main()
 {
     MTK;
@@ -60,6 +44,4 @@ int main()
     cin >> t;
     while (t--)
         solve();
-
-    return 0;
 }
