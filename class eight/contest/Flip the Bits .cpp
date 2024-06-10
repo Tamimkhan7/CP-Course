7b64af208689938ad2d718829d4e8bdaa212703b
Tamim khan <mdtamimkhan842@gmail.com>
2024-06-11 01:03:24 +0600

    modified class eight

--- class eight/contest/Flip the Bits .cpp
+++ class eight/contest/Flip the Bits .cpp
@@ -33,16 +33,16 @@ void solve()
         else
             one_count++;
         prefix[i + 1] = zero_count - one_count;
-        cout << prefix[i + 1] << ' ';
+        // cout << prefix[i + 1] << ' ';
     }
-    cout << '\n';
+    // cout << '\n';
     bool flag = true;
     bool flipped = false;
 
     for (int i = n - 1; i >= 0; i--)
     {
         char current_a = (flipped) ? (a[i] == '0' ? '1' : '0') : a[i];
-        cout << current_a << ' ';
+        // cout << current_a << ' ';
         if (current_a != b[i])
         {
             if (prefix[i + 1] != 0)
@@ -59,13 +59,27 @@ void solve()
     else
         cout << "NO" << '\n';
 }
-
+bool solve1()
+{
+    string a, b;
+    int n;
+    cin >> n >> a >> b;
+    for (int i = 0, cnt = 0; i < n; i++)
+    {
+        cnt += (a[i] == '1' ? 1 : -1);
+        if ((a[i] == b[i]) != (a[i + 1] == b[i + 1]) && cnt)
+            return 0;
+    }
+    return 1;
+}
 int32_t main()
 {
     MTK;
     int t;
     cin >> t;
     while (t--)
-        solve();
+    {
+        cout << (solve1() ? "YES\n" : "NO\n");
+    }
     return 0;
 }
