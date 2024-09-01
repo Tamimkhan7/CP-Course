860fc6a9935216994e045c5aa170d811e7cfd8cb
Tamim khan <mdtamimkhan842@gmail.com>
2024-09-02 01:39:56 +0600

    modified phase 2  class one contest problem chef and tuples and divisor summation

--- Phase 2/Class one/Contest/Chef and Tuples.cpp
+++ Phase 2/Class one/Contest/Chef and Tuples.cpp
@@ -19,7 +19,39 @@ int32_t main()
     {
         int n, a, b, c;
         cin >> n >> a >> b >> c;
-        
+        vector<int> divs;
+        for (int i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                divs.push_back(i);
+                if (i != n / i)
+                    divs.push_back(n / i);
+            }
+        }
+        // for (auto x : divs)
+        //     cout << x << ' ';
+        // cout << '\n';
+        int ans = 0; // x*y*z==n// z = n/(x*y)
+        for (auto x : divs)
+        {
+            if (x <= a)
+            {
+                for (auto y : divs)
+                {
+                    if (y <= b)
+                    {
+                        if (n % (1LL * x * y) == 0)
+                        {
+                            int z = n / (x * y);
+                            if (z <= c)
+                                ans++;
+                        }
+                    }
+                }
+            }
+        }
+        cout << ans << '\n';
     }
     return 0;
 }
--- Phase 2/Class one/Contest/Divisor summation.cpp
+++ Phase 2/Class one/Contest/Divisor summation.cpp
@@ -9,28 +9,54 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define ll int long long
 #define mod 1000000007
-
+const int N = 5e5 + 9;
+ll spf[N], sum_of_div[N];
 int32_t main()
 {
     MTK;
+    // for (int i = 1; i < N; i++)
+    //     spf[i] = i;
+    // for (int i = 2; i < N; i++)
+    // {
+    //     for (int k = i; k < N; k += i)
+    //         spf[k] = min(spf[k], i);
+    // }
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     int x;
+    //     cin >> x;
+    //     int y = x;
+    //     ll sum_of_div = 1; // we know that--p1^e1+p2^e2+p3^e3..........pk^ek  --that is find total sum of divisor -- (p1^e1-1/p1-1)*(p2^e2-1/p2-1)........(pk^ek-1/pk-1)
+    //     while (x > 1)
+    //     {
+    //         int p = spf[x];
+    //         int prime_pow = 1;
+    //         while (x % p == 0)
+    //         {
+    //             prime_pow *= p;
+    //             x /= p;
+    //         }
+    //         sum_of_div *= (1LL * prime_pow * p - 1) / (p - 1);
+    //     }
+    //     cout << sum_of_div - y << '\n';
+    // }
+
+    // using sieve style solve the problem
+
+    for (int i = 1; i < N; i++)
+    {
+        for (int j = i; j < N; j += i)
+            sum_of_div[j] += i;
+    }
     int t;
     cin >> t;
     while (t--)
     {
         int n;
         cin >> n;
-        ll sum = 0;
-        for (int i = 1; i * i <= n; i++)
-        {
-            if (n % i == 0)
-            {
-                sum += i;
-                if ((i != n / i) and (n / i < n))
-                    sum += n / i;
-            }
-        }
-        cout << sum << '\n';
+        cout << sum_of_div[n] - n << '\n';
     }
-
     return 0;
 }
