24a34b550fdff0d5acd0c78259d2f2e178eb9b93
Tamim khan <mdtamimkhan842@gmail.com>
2024-09-21 02:09:26 +0600

    modified digit and factorial and wtf play risky in mathematics and added story of tomisu ghost

--- Phase 2/class two/YouKn0wWho Academy Phase 2: WTF/G. PRIME - Play Risky In MathEmatics.cpp
+++ Phase 2/class two/YouKn0wWho Academy Phase 2: WTF/G. PRIME - Play Risky In MathEmatics.cpp
@@ -5,13 +5,12 @@ using namespace std;
     cin.tie(0);                   \
     cout.tie(0);
 #define mem(a, b) memset(a, b, sizeof(a))
-#define trace(x) cout << #x << ' ' << x << endl
 #define all(x) (x).begin(), (x).end()
-#define ll int long long
-#define mod 1000000007
+#define ll long long
 const int N = 1e6 + 9;
 bool f[N];
 vector<int> prime;
+
 void sieve()
 {
     f[1] = true;
@@ -19,7 +18,6 @@ void sieve()
     {
         if (!f[i])
         {
-            prime.push_back(i);
             for (int j = i * i; j < N; j += i)
                 f[j] = true;
         }
@@ -31,8 +29,10 @@ void sieve()
     }
 }
 
-bool ok(int n)
+bool is_prime(int n)
 {
+    if (n < N)
+        return !f[n];
     for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
@@ -40,33 +40,43 @@ bool ok(int n)
     }
     return true;
 }
+
 int32_t main()
 {
     MTK;
     sieve();
-    // for (auto x : prime)
-    //     cout << x << ' ';
-    // cout << '\n';
+
     int n;
     cin >> n;
-    if (ok(2) and ok(n - 2))
+
+    if (is_prime(n))
+    {
+        cout << 1 << '\n';
+        cout << n << '\n';
+        return 0;
+    }
+
+    if (is_prime(n - 2))
     {
         cout << 2 << '\n';
         cout << 2 << ' ' << n - 2 << '\n';
+        return 0;
     }
-    else
+
+    cout << 3 << '\n';
+    cout << 3 << ' ';
+    n -= 3;
+
+    for (int i = 0; i < prime.size(); i++)
     {
-        cout << 3 << '\n';
-        cout << 3 << ' ';
-        n -= 3;
-        for (int i = 3; i <= sqrt(n); i++)
+        int x = prime[i];
+        int y = n - x;
+        if (y > 0 && is_prime(y))
         {
-            if (ok(i) and ok(n - i))
-            {
-                cout << i << ' ' << n - i << '\n';
-                break;
-            }
+            cout << x << ' ' << y << '\n';
+            return 0;
         }
     }
+
     return 0;
 }
