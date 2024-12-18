333599625a4ad903ad55418ba508071bffe792f0
Tamim khan <mdtamimkhan842@gmail.com>
2024-12-19 01:22:55 +0600

    modified phase 1 class six binary search modified factory machines

--- Phase 1/Class Six/Binary Search/Factory Machines.cpp
+++ Phase 1/Class Six/Binary Search/Factory Machines.cpp
@@ -9,13 +9,16 @@ typedef long long int ll;
 #define all(x) x.begin(), x.end()
 #define mod 1000000007
 const int N = 2e5 + 9;
-vector<int> a(N);
-int n, t;
+int n, t, a[N];
 bool f(ll x)
 {
     ll sum = 0;
-    for (int i = 1; i <= n; i++)
-        sum += a[i] / x;
+    for (int i = 0; i < n; i++)
+    {
+        sum += x / a[i];
+        if (sum >= t)
+            return true;
+    }
 
     return sum >= t;
 }
@@ -23,21 +26,20 @@ int32_t main()
 {
     MTK;
     cin >> n >> t;
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
         cin >> a[i];
-    int mx = *max_element(all(a));
-    sort(a +, a + n + 1);
-    int l = 1, r = 2e5, ans = -1;
-    // while (l < r)
-    // {
-    //     ll mid = (l + r) / 2;
-    //     if (f(mid))
-    //     {
-    //         r = mid - 1;
-    //         ans = mid;
-    //     }
-    //     else
-    //         l = mid + 1;
-    // }
+
+    ll l = 0, r = LLONG_MAX, ans = 0;
+    while (l <= r)
+    {
+        ll mid = l + (r - l) / 2;
+        if (f(mid))
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+            l = mid + 1;
+    }
     cout << ans << '\n';
 }
