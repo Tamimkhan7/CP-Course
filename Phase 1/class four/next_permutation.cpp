a63eb711c2fc5b13ba8b9b33f282cc03e2f529d4
Tamim khan <mdtamimkhan842@gmail.com>
2024-12-20 20:46:13 +0600

    modified next_permutation

--- Phase 1/class four/next_permutation.cpp
+++ Phase 1/class four/next_permutation.cpp
@@ -1,24 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mem(a, b) memset(a, b, sizeof(a))
+#define show(x) cout << #x << ' ' << x << endl
+#define all(x) (x).begin(), (x).end()
+#define ll int long long
+#define mod 1000000007
+
+void print(vector<int> v)
 {
+    for (auto x : v)
+        cout << x << ' ';
+    cout << '\n';
+}
+int32_t main()
+{
+    MTK;
+
     int n;
-    cin>>n;
-    int a[n];
-    for(int i=0; i<n; i++)cin>>a[i];
-//next_permutation//aita building function//porer joto permutation ace sokol permutation print kore//permutation basically hoy lexicographically
-//prev_permutation//aita building function//ager joto permutation ace sokol permutation print kore//permutation basically hoy lexicographically
-    //best holo,, do while loop use kora, tobe arek vabe kora jay,,, just while diye use kora jay
-    int ans=0;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    // next_permutation//aita building function//porer joto permutation ace sokol permutation print kore//permutation basically hoy lexicographically
+    // prev_permutation//aita building function//ager joto permutation ace sokol permutation print kore//permutation basically hoy lexicographically
+    // best holo,, do while loop use kora, tobe arek vabe kora jay,,, just while diye use kora jay
+    int ans = 0;
     do
     {
         ans++;
-        for(int i=0; i<n; i++)
-        {
-            cout<<a[i]<<" ";
-        }
-        cout<<endl;
-    }
-    while(next_permutation(a, a+n));
-    cout<<ans<<endl;
+        print(v);
+    } while (next_permutation(all(v)));
+    cout << ans << '\n';
 }
